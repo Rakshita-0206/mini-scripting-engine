12aab2b72bf0678ff9e9335440c990c86613d9ee
Rakshita K Biradar <2613rakshitakb@gmail.com>
2026-04-02 23:25:46 +0530

    Add files via upload

---
+++ src/project/parser/VariableNode.java
@@ -0,0 +1,14 @@
+package Aoopproject;
+
+public class VariableNode implements Expression {
+    private final String name;
+
+    public VariableNode(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Object evaluate(Environment env) {
+        return env.get(name);  // throws if not defined
+    }
+}
